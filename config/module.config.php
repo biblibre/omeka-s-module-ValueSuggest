bd85080dec471170bb9d336ea9ad7196391f6db6
Julian Maurice <julian.maurice@biblibre.com>
2025-06-17 09:59:50 +0200

    Revert "Merge branch 'value-updater-idref' into biblibre-4.1"
    This reverts commit 80564cafcdfb292de57ca07fd02f2666d9477f62, reversing
    changes made to e2b013cdc48c016b12c172f4cf8975d9591ccaec.

--- config/module.config.php
+++ config/module.config.php
@@ -7,27 +7,6 @@ return [
             'ValueSuggest\Controller\Index' => Service\IndexControllerFactory::class,
         ],
     ],
-    'form_elements' => [
-        'factories' => [
-            Form\UpdateForm::class => Service\Form\UpdateFormFactory::class,
-        ],
-    ],
-    'navigation' => [
-        'AdminModule' => [
-            [
-                'label' => 'Value Suggest',
-                'route' => 'admin/value-suggest/update',
-                'resource' => 'ValueSuggest\Controller\Index',
-                'privilege' => 'update',
-                'pages' => [
-                    [
-                        'label' => 'Update values', // @translate
-                        'route' => 'admin/value-suggest/update',
-                    ],
-                ],
-            ],
-        ],
-    ],
     'translator' => [
         'translation_file_patterns' => [
             [
@@ -319,15 +298,6 @@ return [
                                     ],
                                 ],
                             ],
-                            'update' => [
-                                'type' => 'Literal',
-                                'options' => [
-                                    'route' => '/update',
-                                    'defaults' => [
-                                        'action' => 'update',
-                                    ],
-                                ],
-                            ]
                         ],
                     ],
                 ],
