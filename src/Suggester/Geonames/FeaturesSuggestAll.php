b589b1e1b16c79ff1de1e400039d364bd09a00b5
Thibaud Guillot <thibaud.guillot@biblibre.com>
2026-07-03 14:49:00 +0200

    PHP 8.4 compatibility (rector fix)

--- src/Suggester/Geonames/FeaturesSuggestAll.php
+++ src/Suggester/Geonames/FeaturesSuggestAll.php
@@ -9,7 +9,7 @@ class FeaturesSuggestAll implements SuggesterInterface
     {
         // Load the feature codes TSV file into an array.
         $features = array_map(
-            fn ($line) => str_getcsv($line, "\t"),
+            fn ($line) => str_getcsv($line, "\t", escape: '\\'),
             file(__DIR__ . '/featureCodes_en.txt')
         );
         // Sort the array alphabetically by value.
